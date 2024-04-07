73ff783d8aa5e0aa5f8d2793a3202f3087afa02b
calt-laboratory <christoph_alt@posteo.de>
2024-04-07 18:12:52 +0200

    feat(main): transformed dataclass data model to sqlalchemy data model

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-from dataclasses import dataclass
-from datetime import datetime
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-@dataclass
-class Customer:
-    id: int
-    first_name: str
-    last_name: str
-    email_adress: str
-
-
-@dataclass
-class NoisyRoom:
-    id: int
-    number: str
-    size: int
-    price: int
-
-
-@dataclass
-class Reservation:
-    id: int
-    from_time: datetime
-    to_time: datetime
-    customer: Customer
-    room: NoisyRoom
-    price: int
+@app.get("/")
+def read_root() -> str:
+    return "The server is running!"
