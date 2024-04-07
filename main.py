37df5f25c046ad9e2c2a592fdeaa273dea48eb97
calt-laboratory <christoph_alt@posteo.de>
2024-04-07 17:06:15 +0200

    feat(main): implemented Noisy Room data model

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dataclasses import dataclass
+from datetime import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@dataclass
+class Customer:
+    id: int
+    first_name: str
+    last_name: str
+    email_adress: str
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@dataclass
+class NoisyRoom:
+    id: int
+    number: str
+    size: int
+    price: int
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@dataclass
+class Reservation:
+    id: int
+    from_time: datetime
+    to_time: datetime
+    customer: Customer
+    room: NoisyRoom
+    price: int
